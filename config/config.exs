a6a4826f6e50b3daad76a2da39f2a1c945371ae9
Dhony Silva <dhony.web@gmail.com>
2025-04-27 19:51:47 -0300

    While gen.auth Accounts, the :scopes has been added to config

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :coqueiro, :scopes,
+  user: [
+    default: true,
+    module: Coqueiro.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Coqueiro.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :coqueiro,
   ecto_repos: [Coqueiro.Repo],
   generators: [timestamp_type: :utc_datetime]
